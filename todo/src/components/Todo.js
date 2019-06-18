058ae00ecbf6aee32b5030b017adf3e375cbcc6b
Jose Montero Jr <josemariodev@gmail.com>
2019-06-17 20:17:03 -0500

    initial commit boilerplated

---
+++ todo/src/components/Todo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { newTodo, toggleTodo } from '../actions';
+
+export class Todo extends Component {
+  render() {
+    return <div />;
+  }
+}
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  { newTodo, toggleTodo },
+)(Todo);
